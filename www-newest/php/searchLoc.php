dcd517e722f8e0d9fd682288a423ebe6b1520023
akanarika <ilykn@hotmail.com>
2014-11-14 18:06:34 +0800

    arranged

--- www-newest/php/searchLoc.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo("<div id=\"curLocCard\"><div class=\"titleBackground\"><div id=\"curLocTitle\">name</div></div><div id=\"curLocDescription\">desc</div><div class=\"divPinFalse\"><img class=\"imgPin\" src=\"img/pinIconGray.png\" name=\"0\"></div><div class=\"photoContainer\" id=\"curLocImgContainer\"><img class=\"photo\" id=\"curLocImg\" src=\"img/Akiba.jpg\"></div></div>");
-?>
